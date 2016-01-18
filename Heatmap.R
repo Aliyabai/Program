1c699384b4d5ca8f7e842ec9dd867cb364e01054
Aliya <1326992019@qq.com>
2016-01-18 09:20:10 +0800

    Update Heatmap.R

--- Heatmap.R
+++ Heatmap.R
@@ -28,3 +28,36 @@ p+theme_grey(base_size=base_size) +labs(x="",
 	y="")+scale_x_continuous(expand=c(0,0),labels=rown,
 	breaks=1:length(rown))+opts(axis.ticks=theme_blank(),axis.text.x=theme_text(size=base_size*0.8,
 		angle=90,hjust=0,colour="grey50"),axis.text.y=theme_text(size=base_size*0.8,hjust=1,colour="grey50"))
+
+#############################################
+library("RColorBrewer")
+library("gplots")
+library("pheatmap")
+
+args<-commandArgs(trailingOnly = TRUE)
+input<-args[1]
+
+d<-read.table(input,header=T,stringsAsFactors=FALSE)
+data <- d[,seq(2,ncol(d),1)]
+data
+rownames(data)<-d[,1]
+rownames(data)
+anno <- data.frame(d[,1])
+rownames(anno) <- rownames(data)
+data.m <- data.matrix(data)
+data.n <- as.numeric(data.m)
+vmax <- max(data.n)
+vmin <- min(data.n)
+
+pdf("Editing.new.cor1.pdf",9,9)
+
+pheatmap(data,annotation_row=anno,annotation_col=anno,color = colorRampPalette(c("#daeffc","#FFFFFF","#FFCCCC","#FF9999","#FF6666","#FF3300"))(50),cluster_cols=T,legend_breaks=vmax:vmin,cluster_rows=T,cellwidth = 20, cellheight = 20,show_rownames = T,show_colnames = T,border_color="grey")
+
+dev.off()
+
+pdf("Editing.new.cor2.pdf",9,9)
+
+pheatmap(data,annotation_row=anno,annotation_col=anno,color = colorRampPalette(c("#daeffc","#FFFFFF","#FFCCCC","#FF9999","#FF6666","#FF3300"))(50),cluster_cols=F,legend_breaks=vmax:vmin,cluster_rows=F,cellwidth = 20, cellheight = 20,show_rownames = T,show_colnames = T,border_color="grey")
+
+dev.off()
+
